844dc1d396d0805bdb16eff5e9756dfcbee01d7a
Marc Hoersken <info@marc-hoersken.de>
2019-03-02 17:49:09 +0100

    Kerberos and GSSAPI Authentication is not supported

--- docker-entrypoint.d/02-setup-sshd.sh
+++ docker-entrypoint.d/02-setup-sshd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-sed -i -E 's/^#?((Password|Kerberos|GSSAPI)Authentication)\s.*/\1 no/g' /etc/ssh/sshd_config
+sed -i -E 's/^#?(PasswordAuthentication)\s.*/\1 no/g' /etc/ssh/sshd_config
 sed -i -E 's/^#?(PubkeyAuthentication)\s.*/\1 yes/g' /etc/ssh/sshd_config
 sed -i -E 's/^#?(Subsystem\ssftp\s)/#\1/g' /etc/ssh/sshd_config
