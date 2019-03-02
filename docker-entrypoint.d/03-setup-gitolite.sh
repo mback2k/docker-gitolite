38e38281b2b6d88c5a7a07b097080b21288b5b44
Marc Hoersken <info@marc-hoersken.de>
2019-03-02 17:28:20 +0100

    Use the available su instead of the unavailable runuser

--- docker-entrypoint.d/03-setup-gitolite.sh
+++ docker-entrypoint.d/03-setup-gitolite.sh
@@ -5,11 +5,11 @@ chown -R git:git ~git
 
 if [ ! -f ~git/.ssh/authorized_keys ]; then
     if [ -f "$SSH_KEY" ]; then
-        runuser -u git -- /bin/sh -c 'gitolite setup -pk "$SSH_KEY"'
+        su - git -c 'gitolite setup -pk "$SSH_KEY"'
     else
         echo "Please provide public SSH_KEY for gitolite admin!"
         exit 1
     fi
 else
-    runuser -u git -- /bin/sh -c 'gitolite setup'
+    su - git -c 'gitolite setup'
 fi
