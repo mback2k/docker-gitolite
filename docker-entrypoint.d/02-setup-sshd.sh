26eb049dd7bb7e21bc62c69c392b7207a9f9df66
Marc Hoersken <info@marc-hoersken.de>
2019-03-02 22:12:11 +0100

    Make it possible to specify ssh ListenAddress with SSH_HOST

--- docker-entrypoint.d/02-setup-sshd.sh
+++ docker-entrypoint.d/02-setup-sshd.sh
@@ -4,3 +4,7 @@ set -e
 sed -i -E 's/^#?(PasswordAuthentication)\s.*/\1 no/g' /etc/ssh/sshd_config
 sed -i -E 's/^#?(PubkeyAuthentication)\s.*/\1 yes/g' /etc/ssh/sshd_config
 sed -i -E 's/^#?(Subsystem\ssftp\s)/#\1/g' /etc/ssh/sshd_config
+
+if [ -n "$SSH_HOST" ]; then
+    sed -i -E "s/^#?(ListenAddress)\s.*/\1 $SSH_HOST/g" /etc/ssh/sshd_config
+fi
