77efa66ff377d8edc0b9677c46e5d31068ea4555
Marc Hoersken <info@marc-hoersken.de>
2019-03-02 17:44:32 +0100

    Fix escaping of SSH_KEY environment variable

--- docker-entrypoint.d/03-setup-gitolite.sh
+++ docker-entrypoint.d/03-setup-gitolite.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-set -ex
+set -e
 
 chown -R git:git ~git
 
 if [ ! -f ~git/.ssh/authorized_keys ]; then
     if [ -f "$SSH_KEY" ]; then
-        su - git -c 'gitolite setup -pk "$SSH_KEY"'
+        su - git -c "gitolite setup -pk \"$SSH_KEY\""
     else
         echo "Please provide public SSH_KEY for gitolite admin!"
         exit 1
     fi
 else
-    su - git -c 'gitolite setup'
+    su - git -c "gitolite setup"
 fi
