af3597c2dd8a7b52204bdfccbda17965d80e63bd
Marc Hoersken <info@marc-hoersken.de>
2019-03-03 19:23:27 +0100

    Disable ChallengeResponseAuthentication as well

--- docker-entrypoint.d/02-setup-sshd.sh
+++ docker-entrypoint.d/02-setup-sshd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sed -i -E 's/^#?(PasswordAuthentication)\s.*/\1 no/g' /etc/ssh/sshd_config
+sed -i -E 's/^#?((Password|ChallengeResponse)Authentication)\s.*/\1 no/g' /etc/ssh/sshd_config
 sed -i -E 's/^#?(PubkeyAuthentication)\s.*/\1 yes/g' /etc/ssh/sshd_config
 sed -i -E 's/^#?(Subsystem\ssftp\s)/#\1/g' /etc/ssh/sshd_config
 
